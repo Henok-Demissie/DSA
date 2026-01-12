c7b2ed2e2ab232c80ec26688805178847394e722
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2026-01-12 14:00:56 +0400

    Time: 3 ms (56.12%) | Memory: 19.6 MB (6.35%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -1,39 +1,18 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        res = []
-
-        def backtrack(index, path):
-            # BASE CASE:
-            # If we have 4 parts AND used all characters
-            if len(path) == 4:
-                if index == len(s):
-                    res.append(".".join(path))
-                return
-
-            # Try taking 1 to 3 digits
-            for length in range(1, 4):
-                if index + length > len(s):
-                    break
-
-                part = s[index:index + length]
-
-                # Rule 1: No leading zero
-                if part[0] == '0' and len(part) > 1:
-                    continue
-
-                # Rule 2: Value <= 255
-                if int(part) > 255:
-                    continue
-
-                # Choose
-                path.append(part)
-
-                # Explore
-                backtrack(index + length, path)
-
-                # Un-choose (BACKTRACK)
-                path.pop()
-
-        backtrack(0, [])
-        return res
-        
+        ans = []
+        self.recurse(s, ans, 0, "", 0)
+        return ans
+    
+    def recurse(self, curr, ans, index, temp, count):
+        if count > 4:
+            return
+        if count == 4 and index == len(curr):
+            ans.append(temp)
+        for i in range(1, 4):
+            if index + i > len(curr):
+                break
+            s = curr[index:index+i]
+            if (s.startswith("0") and len(s) > 1) or (i == 3 and int(s) >= 256):
+                continue
+            self.recurse(curr, ans, index+i, temp + s + ("." if count < 3 else ""), count+1)
